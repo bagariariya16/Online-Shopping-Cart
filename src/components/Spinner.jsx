21967b290f32a434b46ace7b7793008ff71d4c84
riya bagaria <bagariaria16@gmail.com>
2024-01-08 21:00:40 +0530

    Initial commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='grid justify-items-center w-[100%] h-screen'>
+        <div className='spinner'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Spinner
